b30be79968eb8096034a8b7e181c377be2620aff
STIVEN2007 <cristianidrobo877@gmail.com>
2025-10-25 11:51:26 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import React, { useState } from "react";
-import Main from "./components/TaskList";
-import TaskForm from "./components/TaskForm";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import React from "react";
 
 function App() {
-  const [user, setUser] = useState(null);
-
-  if (!user) return <Login onLogin={setUser} />;
-
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1>Bienvenido, {user}</h1>
-      <TaskForm onTaskCreated={() => {}} />
-      <Main />
+    <div>
+      <h1>Hola mundo desde React</h1>
     </div>
   );
 }
