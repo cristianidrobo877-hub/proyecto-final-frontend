c4e6454bc146a1fd04781fab42a8f464f64d5480
STIVEN2007 <cristianidrobo877@gmail.com>
2025-10-25 11:54:08 -0500

    Update TaskList.jsx

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -5,12 +5,8 @@ function TaskList() {
   const [tareas, setTareas] = useState([]);
 
   const fetchTareas = async () => {
-    try {
-      const res = await getTareas();
-      setTareas(res.data);
-    } catch (error) {
-      console.error(error);
-    }
+    const res = await getTareas();
+    setTareas(res.data);
   };
 
   useEffect(() => {
@@ -25,9 +21,14 @@ function TaskList() {
   return (
     <ul>
       {tareas.map((t) => (
-        <li key={t.id}>
-          <strong>{t.titulo}</strong> - {t.descripcion}
-          <button onClick={() => handleEliminar(t.id)}>Eliminar</button>
+        <li key={t.id} className="mb-2">
+          <strong>{t.titulo}</strong> - {t.descripcion}{" "}
+          <button
+            onClick={() => handleEliminar(t.id)}
+            className="bg-red-500 text-white p-1 ml-2"
+          >
+            Eliminar
+          </button>
         </li>
       ))}
     </ul>
