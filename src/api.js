8ea9778541e8496a5cac7d4ada6ffc01037358dd
STIVEN2007 <cristianidrobo877@gmail.com>
2025-10-25 11:57:52 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,14 +1,8 @@
-// src/api.js
 import axios from "axios";
 
 const API_URL = "https://proyecto-final-backend-1nh4.onrender.com";
 
 export const API = axios.create({ baseURL: API_URL });
-
-// Tareas
-export const getTareas = () => API.get("/tareas");
-export const createTarea = (tarea) => API.post("/tareas", tarea);
-export const deleteTarea = (id) => API.delete(`/tareas/${id}`);
 
 // Usuarios
 export const registerUser = (data) => API.post("/register", data);
