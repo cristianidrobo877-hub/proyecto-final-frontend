f748a3141f687d96b59a2140ecd5da7b5214069d
STIVEN2007 <cristianidrobo877@gmail.com>
2025-10-25 11:55:40 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
 
 function App() {
+  const user = localStorage.getItem("user");
+
   return (
-    <div>
-      <h1>Hola mundo desde React</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to={user ? "/tareas" : "/login"} />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/tareas"
+          element={
+            user ? (
+              <div style={{ padding: "2rem" }}>
+                <h1>Bienvenido, {user}</h1>
+                <TaskForm onTaskCreated={() => {}} />
+                <TaskList />
+              </div>
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
